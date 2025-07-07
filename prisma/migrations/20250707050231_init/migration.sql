e77d7a9e76277bf45ade4024db029310682ebfb6
Ibnu Topan <99580553+IbnuTopanAdib@users.noreply.github.com>
2025-07-07 12:02:54 +0700

    Init migration for fresh start

--- prisma/migrations/20250707050231_init/migration.sql
+++ prisma/migrations/20250707050231_init/migration.sql
@@ -34,7 +34,8 @@ CREATE TABLE `Visitor` (
     `imgBase64` VARCHAR(191) NOT NULL,
     `type` INTEGER NOT NULL,
     `passtime` VARCHAR(191) NULL,
-    `md5` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
 
     UNIQUE INDEX `Visitor_idcardNum_key`(`idcardNum`),
     PRIMARY KEY (`id`)
@@ -43,8 +44,11 @@ CREATE TABLE `Visitor` (
 -- CreateTable
 CREATE TABLE `Attendance` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `visitorId` INTEGER NOT NULL,
+    `visitorId` INTEGER NULL,
     `deviceId` INTEGER NOT NULL,
+    `groupId` VARCHAR(191) NOT NULL,
+    `recordId` VARCHAR(191) NOT NULL,
+    `imgBase64` VARCHAR(191) NOT NULL,
     `time` DATETIME(3) NOT NULL,
     `type` VARCHAR(191) NOT NULL,
     `extra` JSON NULL,
@@ -54,7 +58,7 @@ CREATE TABLE `Attendance` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
-ALTER TABLE `Attendance` ADD CONSTRAINT `Attendance_visitorId_fkey` FOREIGN KEY (`visitorId`) REFERENCES `Visitor`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `Attendance` ADD CONSTRAINT `Attendance_visitorId_fkey` FOREIGN KEY (`visitorId`) REFERENCES `Visitor`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `Attendance` ADD CONSTRAINT `Attendance_deviceId_fkey` FOREIGN KEY (`deviceId`) REFERENCES `Device`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
