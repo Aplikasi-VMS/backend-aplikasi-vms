677f6f05e62fd4cc9d8d6a48c7746694abef7bf9
Ibnu Topan <99580553+IbnuTopanAdib@users.noreply.github.com>
2025-07-08 05:16:44 +0700

    perbaiki limit file

--- app.js
+++ app.js
@@ -9,8 +9,9 @@ import prisma from "./lib/prisma_client.js";
 
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: '10mb' })); 
+app.use(express.urlencoded({ extended: false, limit: '10mb' }));
+
 app.use(cors({
     origin: true,
     credentials: true,
