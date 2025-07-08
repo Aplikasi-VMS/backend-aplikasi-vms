043bae6e12d68366b4401820f7746fd7b2110e29
Ibnu Topan <99580553+IbnuTopanAdib@users.noreply.github.com>
2025-07-08 09:40:45 +0700

    fix minor error

--- routes/attendance.routes.js
+++ routes/attendance.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { dataUpload } from '../controllers/attendance.controller';
+import { dataUpload } from '../controllers/attendance.controller.js';
 
 const attendanceRouter = express.Router();
 
