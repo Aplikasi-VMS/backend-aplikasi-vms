2e9a1c0e5bc19774ecab3de14f622d8b0b7ed5b8
Ibnu Topan <99580553+IbnuTopanAdib@users.noreply.github.com>
2025-07-10 14:11:43 +0700

    fix upload error

--- controllers/attendance.controller.js
+++ controllers/attendance.controller.js
@@ -1,61 +1,62 @@
 import prisma from '../lib/prisma_client.js';
 
 export const dataUpload = async (req, res, next) => {
-    try {
-        const {
-            groupId,
-            deviceKey,
-            idcardNumber,
-            recordId,
-            imgBase64,
-            time,
-            type,
-            extra
-        } = req.body;
+  try {
+    const {
+      groupId,
+      deviceKey,
+      idcardNumber,
+      recordId,
+      imgBase64,
+      time,
+      type,
+      extra
+    } = req.body;
 
-        const device = await prisma.device.findUnique({
-            where: { deviceKey }
-        });
+    const device = await prisma.device.findUnique({
+      where: { deviceKey }
+    });
 
-        if (!device) {
-            return res.status(400).json({
-                result: 0,
-                success: false,
-                msg: "Invalid deviceKey"
-            });
-        }
-
-        const visitor = await prisma.visitor.findUnique({
-            where: { idcardNum: idcardNumber }
-        });
-
-        await prisma.attendance.create({
-            data: {
-                visitorId: visitor ? visitor.id : null,
-                deviceId: device.id,
-                groupId,
-                recordId,
-                imgBase64,
-                time: new Date(parseInt(time)),
-                type,
-                extra: extra ? JSON.parse(extra) : undefined
-            }
-        });
-
-        res.json({
-            result: 1,
-            success: true,
-            msg: "Diterima dengan sukses"
-        });
-
-    } catch (error) {
-        console.error("Upload attendance error:", error);
-        res.status(500).json({
-            result: 0,
-            success: false,
-            msg: "Failed to save attendance"
-        });
+    if (!device) {
+      return res.status(400).json({
+        result: 0,
+        success: false,
+        msg: "Invalid deviceKey"
+      });
     }
+
+    const visitor = await prisma.visitor.findUnique({
+      where: { idcardNum: idcardNumber }
+    });
+
+
+    await prisma.attendance.create({
+      data: {
+        visitorId: visitor ? visitor.id : null,
+        deviceId: device.id,
+        groupId,
+        recordId,
+        imgBase64,
+        time: new Date(parseInt(time)),
+        type,
+        extra: extra
+      }
+    });
+
+    res.json({
+      result: 1,
+      success: true,
+      msg: "Diterima dengan sukses"
+    });
+
+  } catch (error) {
+    console.error("Upload attendance error:", error);
+    res.status(500).json({
+      result: 0,
+      success: false,
+      msg: "Failed to save attendance"
+    });
+  }
 };
 
 export const getAllAttendances = async (req, res, next) => {
@@ -67,25 +68,25 @@ export const getAllAttendances = async (req, res, next) => {
 
     const whereCondition = search
       ? {
-          OR: [
-            {
-              visitor: {
-                name: {
-                  contains: search,
-                  mode: 'insensitive',
-                },
+        OR: [
+          {
+            visitor: {
+              name: {
+                contains: search,
+                mode: 'insensitive',
               },
             },
-            {
-              device: {
-                name: {
-                  contains: search,
-                  mode: 'insensitive',
-                },
+          },
+          {
+            device: {
+              name: {
+                contains: search,
+                mode: 'insensitive',
               },
             },
-          ],
-        }
+          },
+        ],
+      }
       : {};
 
     const total = await prisma.attendance.count({
